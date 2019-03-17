3ececa61775a265bf91fbd791238d5911d8d0782
Michal Bojanowski <michal.bojanowski@onegini.com>
2019-03-17 13:06:38 +0100

    Refactor weapomn validator

--- src/main/java/pl/sdacademy/ju4practice/WeaponValidator.java
+++ src/main/java/pl/sdacademy/ju4practice/WeaponValidator.java
@@ -1,5 +1,7 @@
 package pl.sdacademy.ju4practice;
 
+import static java.util.Objects.isNull;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,29 +16,46 @@ public class WeaponValidator {
     // każdu jest użyty tylko raz
 
     public boolean isValid(final Weapon weapon) {
+        validateBaseDamage(weapon);
+        validateName(weapon);
+        validateArmorTypes(weapon);
+        return true;
+    }
 
-        if (weapon.getBaseDamage() == null) {
+    private void validateBaseDamage(final Weapon weapon) {
+        if (isNull(weapon.getBaseDamage())) {
             throw new InvalidWeaponException("Weapon base damage should not be null");
         }
 
         if (weapon.getBaseDamage() < 0) {
             throw new InvalidWeaponException("Base damage should be positive");
         }
+    }
 
+    private void validateName(final Weapon weapon) {
         if (weapon.getName().isEmpty() || weapon.getName().length() < 3) {
             throw new
                 InvalidWeaponException("Weapon name should be longer than 3 chars");
         }
+    }
 
+    private List<ArmorType> getListOfAllArmorTypesInWeapon(final Weapon weapon) {
         final List<ArmorType> listOfAllArmorTypesInWeapon = new ArrayList<>();
         listOfAllArmorTypesInWeapon.addAll(weapon.getGoodAgainst());
         listOfAllArmorTypesInWeapon.addAll(weapon.getBadAgainst());
         listOfAllArmorTypesInWeapon.addAll(weapon.getNeutralAgainst());
+        return listOfAllArmorTypesInWeapon;
+    }
 
-        final List<ArmorType> expectedArmorType = Arrays.asList(ArmorType.values());
+    private List<ArmorType> getAllArmorTypesList() {
+        return Arrays.asList(ArmorType.values());
+    }
 
-        final boolean allArmorTypesCorrect = expectedArmorType.stream()
-            .allMatch(type -> listOfAllArmorTypesInWeapon.contains(type));
+    private void validateArmorTypes(final Weapon weapon) {
+        final List<ArmorType> listOfAllArmorTypesInWeapon = getListOfAllArmorTypesInWeapon(weapon);
+        final List<ArmorType> expectedArmorType = getAllArmorTypesList();
+
+        final boolean allArmorTypesCorrect = listOfAllArmorTypesInWeapon.containsAll(expectedArmorType);
 
         if (!allArmorTypesCorrect) {
             throw new InvalidWeaponException("Armor type missing");
@@ -45,7 +64,5 @@ public class WeaponValidator {
         if (listOfAllArmorTypesInWeapon.size() != expectedArmorType.size()) {
             throw new InvalidWeaponException("Armor type duplicated");
         }
-
-        return true;
     }
 }
